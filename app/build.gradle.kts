5add22dbe048ee107e9560c55370927e27bd3015
rob-builds-future <robin-bettinghausen@gmx.de>
2025-07-07 20:11:44 +0200

    Calendar View UI Detail

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,7 +18,7 @@ android {
         applicationId = "com.rbf.echojournal"
         minSdk = 24
         targetSdk = 35
-        versionCode = 1
+        versionCode = 2
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
